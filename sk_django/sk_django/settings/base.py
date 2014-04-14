1f65e24d17a7f402b6535f24c88356a03973a952
Damian Perez <damian.adriel@gmail.com>
2014-04-14 00:35:23 -0500

    CHG: Changes.

--- sk_django/sk_django/settings/base.py
+++ sk_django/sk_django/settings/base.py
@@ -80,3 +80,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
 STATIC_URL = '/static/'
+
+MEDIA_ROOT = os.sep.join(os.path.abspath(__file__).split(os.sep)[:-2] + ['media'])
+MEDIA_URL = '/media/'
